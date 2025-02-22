4b830cbf2edadc7f0030e1d424a3eb31edc0db84
Noel Caponpon <caponponnoel_bsit@plmun.edu.ph>
2025-02-22 14:23:05 +0800

    Submitting Python Week 05 - Laboratory # 19

--- grade_calculator_functions.py
+++ grade_calculator_functions.py
@@ -11,7 +11,7 @@ def problem_one(score):
         return 'F'
         
 score = float(input("Enter your score: "))
-if 0 <= score <= 100:
- print(f"Your Grade is: {problem_one(score)}")
-else:
- print("Invalid")
+    if 0 <= score <= 100:
+        print(f"Your Grade is: {problem_one(score)}")
+    else:
+        print("Invalid")
